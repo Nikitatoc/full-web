ef63f2bb633ff4800f277adbfc7e0135e3c12157
Nikitatoc <94145387+Nikitatoc@users.noreply.github.com>
2022-08-12 16:04:39 +0300

    Create cabinet.php

---
+++ cabinet.php
@@ -0,0 +1,10 @@
+<?php
+require "header.php"
+?>
+<p class="text">Як що в вас виникли питання напишіть нам на єлектронну адресу у розділі 'Контакти'</p>
+<form action="go out.php">
+<button id="go out" type="submit">Вихід</button>
+</form>
+<?php
+require "footer.php"
+?>
