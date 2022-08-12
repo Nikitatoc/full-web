5e9bd3af1d1f4c7f31d493597d1a7983830b31ef
Nikitatoc <94145387+Nikitatoc@users.noreply.github.com>
2022-08-12 15:59:20 +0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+<h2>Наші проєкти</h2>
+    <a href="https://github.com/Nikitatoc/Notepad.git" class="href">notepad</a>
+    <a href="https://github.com/Nikitatoc/MyProjects.git" class="href">test</a>
+    <a href="https://github.com/Nikitatoc/project.html.git" class="href">capybara</a>
+</footer>
+
+</body>
+</html>
