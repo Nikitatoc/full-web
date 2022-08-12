8db1298364571540fa17e668a619ec145269b36c
Nikitatoc <94145387+Nikitatoc@users.noreply.github.com>
2022-08-12 15:55:36 +0300

    Create login.php

---
+++ login.php
@@ -0,0 +1,11 @@
+<?php
+require "header.php"
+?>
+<form id="login form" method="post" action="cookie.php">
+    <input type="text" name="login" placeholder="Введіть ваш логін" class="style"><br>
+    <input type="password" name="password" placeholder="Введіть ваш пароль" class="style"><br>
+    <input type="submit" placeholder="Увійти" id="login button">
+</form>
+<?php
+require "footer.php"
+?>
